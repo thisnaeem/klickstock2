fe6f426d26186a0d4d102067a6764cbafd9af066
klickstock <klickstock4748@gmail.com>
2025-08-03 19:12:10 -0700

    Update middleware.ts
    Exclude sitemap.xml from Middleware

--- middleware.ts
+++ middleware.ts
@@ -72,7 +72,7 @@ export default auth((req) => {
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!_next|sitemap.xml|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
